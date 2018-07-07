e9d0f4d18e123ba0f85e9b5dd8465277846126c4
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-07-07 17:44:18 +0100

    Added member abstract class

---
+++ src/main/java/db/Member.java
@@ -0,0 +1,79 @@
+package db;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+
+public abstract class Member {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double money;
+    List<Episode> episodes;
+
+    public Member(String firstName, String lastName, double money) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.money = money;
+    }
+
+    public Member(){
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name="lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name="money")
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @ManyToMany
+    @JoinTable(name="episode_member", joinColumns = {@JoinColumn(name="member_id", nullable = false, updatable =false)},
+    inverseJoinColumns =  {@JoinColumn(name = "episode_id", nullable = false, updatable = false)})
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(List<Episode> episodes) {
+        this.episodes = episodes;
+    }
+}
